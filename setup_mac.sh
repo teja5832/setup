89c952a2c2df9f213bd03fff7e7b0d3c2354ecf9
Surya Teja Chavali <teja.coolrocks@gmail.com>
2019-05-11 15:04:50 -0500

    Initial Commit

---
+++ setup_mac.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+ssh-keygen
+
+xcode-select --install
+
+if [[ -z `which brew` ]]
+then
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install git wget axel bash zsh cmake bazel colordiff ffmpeg djvulibre gnuplot graphviz htop pdfgrep pdsh csshx qpdf source-highlight xz ansible #python
+
+# Python 3.6 for now
+brew install --ignore-dependencies https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+sudo pip3 install --upgrade ipython[all]
+pip3 install --upgrade jupyter pytest numpy scipy matplotlib sklearn tensorflow torch torchvision tensorflow-tensorboard keras hyperopt 
+pip3 install --upgrade pylint youtube-dl jedi json-logging numba networkx pandas psutil rope mypy virtualenv scanf coverage
+# pip3 install --upgrade dask diskarray chest nltk spacy gym sympy q snakeviz 
+# python3 -c "import nltk ; nltk.download('all')"
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+cp ~/.zshrc ~/.zshrc.bak
+cp .zshrc_mac ~/.zshrc
+
+cp .bash_aliases_mac ~/.bash_aliases
+
+echo Done with bashrc stuff!
+
+brew cask install firefox brave-browser slack kindle send-to-kindle vlc sublime-text iterm2 telgram-desktop xquartz skype zoomus
+brew cask instal mactex lyx 
+
+#see https://joernhees.de/blog/2014/02/25/scientific-python-on-mac-os-x-10-9-with-homebrew/
